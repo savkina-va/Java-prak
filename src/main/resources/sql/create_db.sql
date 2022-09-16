2f5777c60b80cb372983e79c25b9145f30a90ce0
Mayer Vera <mayerv.a@yandex.ru>
2022-09-16 12:47:50 +0300

    create bd

--- src/main/resources/sql/create_db.sql
+++ src/main/resources/sql/create_db.sql
@@ -1,64 +1,59 @@
-DROP TABLE IF EXISTS client CASCADE;
-CREATE TABLE client
+DROP TABLE IF EXISTS personal_accounts CASCADE;
+CREATE TABLE personal_accounts
 (
-    client_id        SERIAL PRIMARY KEY,
-    type_of_clint    varchar(50) NOT NULL,
-    name_cliet       text        NOT NULL,
-    surname          text        NULL,
-    middle_name      text        NULL,
-    telephone_number varchar(15) NOT NULL,
-    email            text        NOT NULL,
-    balance          integer     NOT NULL,
-    credit_limit     integer     NOT NULL
+    client_id     SERIAL PRIMARY KEY,
+    type_of_clint varchar(50) NOT NULL,
+    name          text        NOT NULL,
+    surname       text        NULL,
+    middle_name   text        NULL,
+    email         text        NOT NULL
 );
 
+COPY personal_accounts (type_of_clint, name, surname, middle_name, email)
+    FROM '/home/vera/Documents/msu/6Sem/web/Web-prak/src/main/resources/sql/personal_accounts.csv'
+    csv HEADER;
 
+DROP TABLE IF EXISTS mobile_accounts CASCADE;
+CREATE TABLE mobile_accounts
+(
+    client_id        integer REFERENCES personal_accounts,
+    telephone_number varchar(50) PRIMARY KEY,
+    balance          integer NOT NULL
+);
 
-COPY client (type_of_clint, name_cliet, surname, middle_name, telephone_number, email, balance, credit_limit)
-    FROM '/home/vera/Documents/msu/6Sem/web/Web-prak/src/main/resources/sql/client.csv'
+COPY mobile_accounts (client_id, telephone_number, balance)
+    FROM '/home/vera/Documents/msu/6Sem/web/Web-prak/src/main/resources/sql/mobile_accounts.csv'
     csv HEADER;
 
 
-DROP TABLE IF EXISTS debit CASCADE;
-CREATE TABLE debit
+DROP TABLE IF EXISTS transactions CASCADE;
+CREATE TABLE transactions
 (
-    debit_id            SERIAL PRIMARY KEY,
-    contract_id         integer REFERENCES contract_history,
-    amount              integer                             NOT NULL,
-    date_of_transaction date                                NOT NULL
+    transaction_id      SERIAL PRIMARY KEY,
+    type_of_transaction varchar(8) NOT NULL,
+    mobile_account      varchar(50)  REFERENCES mobile_accounts,
+    amount              integer     NOT NULL,
+    date_of_transaction date        NOT NULL
 );
 
-COPY debit (contract_id, amount, date_of_transaction)
-    FROM '/home/vera/Documents/msu/6Sem/web/Web-prak/src/main/resources/sql/debit.csv'
-    csv HEADER;
-
-DROP TABLE IF EXISTS deposit CASCADE;
-CREATE TABLE deposit
-(
-    deposit_id          SERIAL PRIMARY KEY,
-    client_id           integer REFERENCES client,
-    amount              integer NOT NULL,
-    date_of_transaction date    NOT NULL
-);
-
-COPY deposit (client_id, amount, date_of_transaction)
-    FROM '/home/vera/Documents/msu/6Sem/web/Web-prak/src/main/resources/sql/deposit.csv'
+COPY transactions (type_of_transaction, mobile_account, amount, date_of_transaction)
+    FROM '/home/vera/Documents/msu/6Sem/web/Web-prak/src/main/resources/sql/transactions.csv'
     csv HEADER;
 
 DROP TABLE IF EXISTS service CASCADE;
 CREATE TABLE service
 (
     service_id        SERIAL PRIMARY KEY,
-    name_service      text        NOT NULL,
-    number_of_devices integer     NOT NULL,
-    minutes           real        NOT NULL,
-    sms               integer     NOT NULL,
-    internet          real        NOT NULL,
-    tariff_fee        integer     NOT NULL,
-    billing_period    varchar(10) NOT NULL -- month, year
+    name_service      text          NOT NULL,
+    number_of_devices integer       NOT NULL,
+    minutes           real          NOT NULL,
+    sms               integer       NOT NULL,
+    internet          real          NOT NULL,
+    unlimited_apps    varchar(50)[] NULL,
+    tariff_fee        integer       NOT NULL
 );
 
-COPY service (name_service, number_of_devices, minutes, sms, internet, tariff_fee, billing_period)
+COPY service (name_service, number_of_devices, minutes, sms, internet, tariff_fee, unlimited_apps)
     FROM '/home/vera/Documents/msu/6Sem/web/Web-prak/src/main/resources/sql/service.csv'
     csv HEADER;
 
@@ -66,13 +61,14 @@ DROP TABLE IF EXISTS contract_history CASCADE;
 CREATE TABLE contract_history
 (
     contract_id       SERIAL PRIMARY KEY,
-    client_id         integer REFERENCES client,
+    mobile_account    varchar(50)  REFERENCES mobile_accounts,
     service_id        integer REFERENCES service,
     telephone_numbers varchar(15)[] NOT NULL,
     date_start        date          NOT NULL,
     date_stop         date          NOT NULL
 );
 
-COPY contract_history (client_id, service_id, telephone_numbers, date_start, date_stop)
+COPY contract_history (mobile_account, service_id, telephone_numbers, date_start, date_stop)
     FROM '/home/vera/Documents/msu/6Sem/web/Web-prak/src/main/resources/sql/contract_history.csv'
     csv HEADER;
+
